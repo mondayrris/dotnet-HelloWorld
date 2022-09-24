6d2debf541d868a3be36ecd162652b09d9599abf
Mondayrris <56475489+mondayrris@users.noreply.github.com>
2022-09-24 15:25:07 +0800

    Create simple HelloWorld console app

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Main is the application entry point, the method that's called automatically by the runtime when it launches the application.
+            // Any command-line arguments supplied when the application is launched are available in the args array.
+            Console.WriteLine("Hello World!");
+
+            // Press ⌥⌘↵ (option+command+enter) to run the app without debugging.
+            // Close the Terminal window.
+        }
+    }
+}
